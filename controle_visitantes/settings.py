3e87af4aa9cbc977804afed40f71298c25aeb577
Pedro Adriano <pedrothiagoadriano@gmail.com>
2021-08-03 21:44:28 -0300

    feat: add login

--- controle_visitantes/settings.py
+++ controle_visitantes/settings.py
@@ -120,3 +120,6 @@ STATIC_URL = "/static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+LOGIN_URL = "login"
+LOGIN_REDIRECT = "index"
