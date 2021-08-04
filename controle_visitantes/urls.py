812c0a032e53487a035c24fda986d01131e414e3
Pedro Adriano <pedrothiagoadriano@gmail.com>
2021-08-03 21:44:08 -0300

    feat: add urls

--- controle_visitantes/urls.py
+++ controle_visitantes/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from dashboard.views import index
 from visitantes.views import (
@@ -11,6 +12,16 @@ from visitantes.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="login.html"),
+        name="login",
+    ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(template_name="logout.html"),
+        name="logout",
+    ),
     path("", index, name="index"),
     path("registrar-visitante/", registrar_visitante, name="registrar_visitante"),
     path("visitantes/<int:id>/", informacoes_visitante, name="informacoes_visitante"),
